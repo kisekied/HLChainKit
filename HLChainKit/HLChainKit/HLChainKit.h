6d7bce96bfa344457d233d9249f47e6d6e8a80eb
kisekied <kisekied@gmail.com>
2017-09-30 14:04:36 +0800

    Add NSMutableString Chain syntax

---
+++ HLChainKit/HLChainKit/HLChainKit.h
@@ -0,0 +1,15 @@
+//
+//  HLChainKit.h
+//  HLChainKit
+//
+//  Created by kisekied on 2017/9/30.
+//  Copyright © kisekied.
+//
+
+#ifndef HLChainKit_h
+#define HLChainKit_h
+
+#import "UIBezierPath+HLChainKit.h"
+#import "NSMutableAttributedString+HLChainKit.h"
+
+#endif /* HLChainKit_h */
--- HLChainKit/ViewController.m
+++ HLChainKit/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-#import "UIBezierPath+HLChainKit.h"
+#import "HLChainKit.h"
 
 @interface ViewController ()
 
